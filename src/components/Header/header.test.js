a45ec542465cc1ae23eed32535b4dd79820268cc
Martin <M.borba98@hotmail.com>
2021-09-14 21:19:09 -0300

    agrego test de ruteo

--- src/components/Header/header.test.js
+++ src/components/Header/header.test.js
@@ -2,16 +2,26 @@ import { render, screen, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import Header from "./index";
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { MemoryRouter } from "react-router-dom";
+it("navigates home when you click the logo", (async) => {
+  // in a real test a renderer like "@testing-library/react"
+  // would take care of setting up the DOM elements
+  const root = document.createElement("div");
+  document.body.appendChild(root);
 
-describe("header component tests", () => {
-  it("renders correctly", () => {
-    const { container } = render(
-      <Router>
-        <Header />
-      </Router>
-    );
-    // Matches the last snapshot taken
-    expect(container).toMatchSnapshot();
-  });
+  // Render app
+  const { container } = render(
+    <MemoryRouter initialEntries={["/"]}>
+      <Header />
+    </MemoryRouter>,
+    root
+  );
+  // Matches the last snapshot taken
+  expect(container).toMatchSnapshot();
+
+  fireEvent(screen.getByRole("submit"), new MouseEvent("click"));
+
+  expect(screen.queryByText("No se pudo iniciar sesión")).toBeVisible();
+  // Click it
+  goHomeLink.dispatchEvent(new MouseEvent("click", { bubbles: true }));
 });
