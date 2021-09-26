0c8ebb99a4ecf26dfe0e719750c994adc2a1c5ad
martin.borba <m.borba98@gmail.com>
2021-09-26 16:12:22 -0300

    agregar y editar persona parece que andando, falta testear y mejorar el pasaje de datos en editar persona

--- src/components/Personas/index.js
+++ src/components/Personas/index.js
@@ -1,31 +1,65 @@
 import * as React from "react";
 import { DataGrid } from "@mui/x-data-grid";
-import { FormControlLabel, IconButton } from "@material-ui/core";
+import { FormControlLabel, IconButton, Box } from "@material-ui/core";
+import Modal from "@material-ui/core/Modal";
 import Button from "@material-ui/core/Button";
+
 import EditIcon from "@material-ui/icons/Edit";
 import DeleteIcon from "@material-ui/icons/Delete";
 import PropTypes from "prop-types";
-
+import { useStyles } from "./styles";
+import CreatePerson from "../../containers/CreatePerson";
+import EditPerson from "../../containers/EditPerson";
 Personas.propTypes = {
   rows: PropTypes.array,
 };
 
 const Acciones = () => {
-  const handleEditClick = () => {
-    // aca para editar la info
+  const [openEdit, setOpenEdit] = React.useState(false);
+  const [personData, setPersonData] = React.useState({});
+  const handleEditOpen = (e) => {
+    let htmlRow =
+      e.target.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode
+        .parentNode;
+    let dataElements = htmlRow.children;
+    let fullName = dataElements[1].innerHTML.split(" ");
+    setPersonData({
+      id: dataElements[0].innerHTML,
+      first_name: fullName[0],
+      last_name: fullName[1],
+      email: dataElements[2].innerHTML,
+      working_hours: dataElements[3].innerHTML,
+    });
+    setOpenEdit(true);
+  };
+  const handleEditClose = () => {
+    setOpenEdit(false);
+    window.location.reload();
   };
 
   const handleRemoveClick = () => {
     // aca para borrar la persona
   };
-
+  const classes = useStyles();
   return (
     <div>
       <FormControlLabel
         control={
-          <IconButton onClick={handleEditClick}>
-            <EditIcon style={{ color: "rgb(30, 30, 30)" }} />
-          </IconButton>
+          <>
+            <IconButton onClick={handleEditOpen}>
+              <EditIcon style={{ color: "rgb(30, 30, 30)" }} />
+            </IconButton>
+            <Modal
+              open={openEdit}
+              onClose={handleEditClose}
+              aria-labelledby="modal-modal-title"
+              aria-describedby="modal-modal-description"
+            >
+              <Box className={classes.modal}>
+                <EditPerson personData={personData} id={personData.id} />
+              </Box>
+            </Modal>
+          </>
         }
       />
       <FormControlLabel
@@ -41,13 +75,18 @@ const Acciones = () => {
 
 const columns = [
   {
+    field: "id",
+    headerName: "ID",
+    //id
+  },
+  {
     field: "fullName",
     headerName: "Nombre completo",
     sortable: true,
     flex: 1, //tamaño
   },
   {
-    field: "id",
+    field: "email",
     headerName: "Email",
     sortable: true,
     flex: 1,
@@ -79,6 +118,14 @@ const columns = [
 ];
 
 export default function Personas({ rows }) {
+  const [openNew, setOpenNew] = React.useState(false);
+  const handleNewOpen = () => setOpenNew(true);
+  const handleNewClose = () => {
+    setOpenNew(false);
+    window.location.reload();
+  };
+
+  const classes = useStyles();
   return (
     <div
       style={{
@@ -95,16 +142,12 @@ export default function Personas({ rows }) {
           margin: 10,
         }} /* relleno, si alguien sabe hacer esto mejor que lo cambie*/
       ></div>
-      <Button
-        color="primary"
-        variant="contained"
-
-      >
+      <Button color="primary" variant="contained" onClick={handleNewOpen}>
         Agregar Persona
       </Button>
       <Modal
-        open={open}
-        onClose={handleClose}
+        open={openNew}
+        onClose={handleNewClose}
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
