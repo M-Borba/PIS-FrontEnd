e6bf0cac041bd253bba5ec538ea65adfddeabdd5
Stefano Graziani <stefa99graziani@gmail.com>
2021-10-09 20:25:17 -0300

    fix horas negativas en personForm

--- src/components/PersonForm/index.js
+++ src/components/PersonForm/index.js
@@ -72,6 +72,7 @@ export default function PersonForm({
           onChange={onInputChange}
         />
         <TextField
+          inputProps={{ min: 0 }}
           variant="outlined"
           margin="normal"
           required
