1e89f2f682fbd73786ba521665c226b98adf2530
joacopc <joaquinpou.ief@gmail.com>
2021-10-20 21:13:11 -0300

    Correcciones solicitadas por el cliente en timeline

---
+++ src/components/Switcher/index.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import Grid from "@material-ui/core/Grid";
+import Switch from "@material-ui/core/Switch";
+import PropTypes from "prop-types";
+
+Switcher.propTypes = {
+    onSwitch: PropTypes.func,
+    isProjectView: PropTypes.bool,
+};
+
+export default function Switcher({ onSwitch, isProjectView }) {
+    return (
+        <>
+            <Grid container spacing={2} justifyContent="center" alignItems="center">
+                Proyectos
+                <Switch color="default" checked={!isProjectView} onChange={onSwitch} />
+                Personas
+            </Grid>
+        </>
+    );
+}
