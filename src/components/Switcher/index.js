7c38837fd9b41630439a0544967fa823dc207d8e
joacopc <joaquinpou.ief@gmail.com>
2021-10-21 11:00:22 -0300

    change switcher color

--- src/components/Switcher/index.js
+++ src/components/Switcher/index.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Grid from "@material-ui/core/Grid";
 import Switch from "@material-ui/core/Switch";
 import PropTypes from "prop-types";
+import Typography from "@mui/material/Typography";
 
 Switcher.propTypes = {
   onSwitch: PropTypes.func,
@@ -12,9 +13,14 @@ export default function Switcher({ onSwitch, isProjectView }) {
   return (
     <>
       <Grid container spacing={2} justifyContent="center" alignItems="center">
-        Proyectos
+        <Typography variant="body1" color="common.white">
+          Proyectos
+        </Typography>
         <Switch color="default" checked={!isProjectView} onChange={onSwitch} />
-        Personas
+        <Typography variant="body1" color="common.white">
+          Personas
+        </Typography>
+
       </Grid>
     </>
   );
