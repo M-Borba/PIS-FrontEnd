2328b337515783dafd0c88d09ed141a4d0c86206
M-Borba <m.borba98@hotmail.com>
2021-10-23 14:25:14 -0300

    cambio a backend staging y centrado de switch timeline

--- src/components/Switcher/index.js
+++ src/components/Switcher/index.js
@@ -11,16 +11,21 @@ Switcher.propTypes = {
 
 export default function Switcher({ onSwitch, isProjectView }) {
   return (
-    <>
-      <Grid container spacing={2} justifyContent="center" alignItems="center">
-        <Typography variant="body1" color="common.white">
-          Proyectos
+    <div style={{ margin: "1vh 1vh 0 1vh" }}>
+      <Grid container
+        direction="row"
+        alignItems="center"
+        justifyContent="center"
+
+        spacing={0} >
+        <Typography variant="body2" color="common.white">
+          Proyecto
         </Typography>
         <Switch color="default" checked={isProjectView} onChange={onSwitch} />
-        <Typography variant="body1" color="common.white">
+        <Typography variant="body2" color="common.white">
           Personas
         </Typography>
       </Grid>
-    </>
+    </div>
   );
 }
