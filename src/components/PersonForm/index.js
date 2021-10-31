bce8163b77a3457d20ea21e2c76438e712c2b5ce
Juan Baz <juan@nayya.com>
2021-10-31 17:40:54 -0300

    errors & technologies

--- src/components/PersonForm/index.js
+++ src/components/PersonForm/index.js
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import propTypes from "prop-types";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
-import Box from "@material-ui/core/Box";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
+import { axiosInstance } from "../../config/axios";
+import Box from "@material-ui/core/Box";
 import { useStyles } from "./styles";
 import TechnologyForm from "../TechnologyForm";
 
@@ -17,6 +18,7 @@ PersonForm.propTypes = {
     last_name: propTypes.string,
     email: propTypes.string,
     working_hours: propTypes.number,
+    technologies: propTypes.array,
   }).isRequired,
   errors: propTypes.object,
   title: propTypes.string,
@@ -31,11 +33,60 @@ export default function PersonForm({
   setErrors,
 }) {
   const classes = useStyles();
+  const [tech, setTech] = useState({ technology: "", seniority: "" });
+  const [technologiesError, setTechnologiesError] = useState({});
+  const [defaultTechs, setDefaultTechs] = useState([]);
 
-  const onInputChange = e => {
+  useEffect(() => {
+    axiosInstance
+      .get("/technologies")
+      .then((response) => {
+        const techs = response.data.technologies?.map((t) => t.name);
+        const personTechs = person.technologies?.map((t) => t[0]);
+        const filteredTechs = techs?.filter((t) => !personTechs.includes(t));
+        setDefaultTechs(filteredTechs);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  const onInputChange = (e) => {
+    setTechnologiesError({});
     setErrors({});
-    setPerson(prev => ({ ...prev, [e.target.name]: e.target.value }))
-  }
+    setPerson((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleAddTechnology = () => {
+    const errors = {};
+    if (!tech.technology) {
+      errors.technology = "No puede ser vacío";
+    }
+    if (!tech.seniority) {
+      errors.seniority = "No puede ser vacío";
+    }
+    if (Object.values(errors).length) {
+      setTechnologiesError(errors);
+      return;
+    }
+
+    const newDefaultTechs = defaultTechs.filter((t) => t !== tech.technology);
+    setDefaultTechs(newDefaultTechs);
+    const { technology, seniority } = tech;
+    const techs = person.technologies;
+    techs.push([technology, seniority]);
+    setTech({ technology: "", seniority: "" });
+    setPerson((prev) => ({ ...prev, technologies: techs }));
+  };
+
+  const handleRemoveTechnology = (tech) => {
+    const newSelectedTechs = person.technologies?.filter(
+      (t) => t[0] !== tech[0]
+    );
+    setPerson((prev) => ({ ...prev, technologies: newSelectedTechs }));
+    defaultTechs.push(tech[0]);
+    setDefaultTechs(defaultTechs);
+  };
 
   return (
     <div className={classes.paper}>
@@ -43,75 +94,88 @@ export default function PersonForm({
         {title}
       </Typography>
       <form className={classes.form} noValidate onSubmit={onSubmit}>
-        <Grid container mt={3} spacing={2}>
-          <Grid item xs={12} md={6}>
-            <TextField
-              variant="outlined"
-              fullWidth
-              id="first_name"
-              name="first_name"
-              type="text"
-              label="Nombre"
-              name="first_name"
-              value={person.first_name}
-              onChange={onInputChange}
-              error={!!errors?.first_name}
-              helperText={errors?.first_name?.[0]}
-            />
+        <Box mt={3}>
+          <Grid container mt={3} spacing={2}>
+            <Grid item xs={12} md={6}>
+              <TextField
+                variant="outlined"
+                fullWidth
+                id="first_name"
+                name="first_name"
+                type="text"
+                label="Nombre"
+                name="first_name"
+                value={person.first_name}
+                onChange={onInputChange}
+                error={!!errors?.first_name}
+                helperText={errors?.first_name?.[0]}
+              />
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <TextField
+                variant="outlined"
+                fullWidth
+                id="last_name"
+                name="last_name"
+                type="text"
+                label="Apellidos"
+                name="last_name"
+                value={person.last_name}
+                onChange={onInputChange}
+                error={!!errors?.last_name}
+                helperText={errors?.last_name?.[0]}
+              />
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                type="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                value={person.email}
+                onChange={onInputChange}
+                error={!!errors?.email}
+                helperText={errors?.email?.[0]}
+              />
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <TextField
+                inputProps={{ min: 0, max: 168 }}
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                name="working_hours"
+                label="Horas Semanales"
+                type="number"
+                id="working_hours"
+                value={person.working_hours}
+                onChange={onInputChange}
+                error={!!errors?.working_hours}
+                helperText={errors?.working_hours?.[0]}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TechnologyForm
+                defaultTechs={defaultTechs}
+                technologies={person.technologies}
+                addTechnology={handleAddTechnology}
+                removeTechnology={handleRemoveTechnology}
+                error={!!errors?.technologies}
+                setTechnologiesError={setTechnologiesError}
+                technologiesError={technologiesError}
+                setAllErrors={setErrors}
+                tech={tech}
+                setTech={setTech}
+              />
+            </Grid>
           </Grid>
-          <Grid item xs={12} md={6}>
-            <TextField
-              variant="outlined"
-              fullWidth
-              id="last_name"
-              name="last_name"
-              type="text"
-              label="Apellidos"
-              name="last_name"
-              value={person.last_name}
-              onChange={onInputChange}
-              error={!!errors?.last_name}
-              helperText={errors?.last_name?.[0]}
-            />
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              type="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              value={person.email}
-              onChange={onInputChange}
-              error={!!errors?.email}
-              helperText={errors?.email?.[0]}
-            />
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <TextField
-              inputProps={{ min: 0, max: 168 }}
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              name="working_hours"
-              label="Horas Semanales"
-              type="number"
-              id="working_hours"
-              value={person.working_hours}
-              onChange={onInputChange}
-              error={!!errors?.working_hours}
-              helperText={errors?.working_hours?.[0]}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <TechnologyForm />
-          </Grid>
-        </Grid>
+        </Box>
         <Button
           role="submit"
           type="submit"
--- src/components/TechnologyForm/index.js
+++ src/components/TechnologyForm/index.js
@@ -1,11 +1,10 @@
-import React, { memo, useState, useEffect } from "react";
+import React, { useMemo } from "react";
 import PropTypes from "prop-types";
-import { axiosInstance } from "../../config/axios";
 import Select from "@material-ui/core/Select";
 import MenuItem from "@material-ui/core/MenuItem";
 import IconButton from "@mui/material/IconButton";
 import AddIcon from "@mui/icons-material/Add";
-
+import Typography from "@material-ui/core/Typography";
 import MuiChip from "@material-ui/core/Chip";
 import Box from "@material-ui/core/Box";
 import Grid from "@material-ui/core/Grid";
@@ -15,119 +14,102 @@ import FormHelperText from "@material-ui/core/FormHelperText";
 import { useStyles } from "./styles";
 import randomColor from "randomcolor";
 
-export default function TechnologyForm({ }) {
-  const classes = useStyles();
-  const [defaultTechs, setDefaultTechs] = useState([]);
-  const [selectedTechs, setSelectedTechs] = useState([]);
-  const [value, setValue] = useState({ technology: "", seniority: "" });
-  const [errors, setErrors] = useState({});
+TechnologyForm.propTypes = {
+  defaultTechs: PropTypes.array,
+  technologies: PropTypes.array,
+  addTechnology: PropTypes.func,
+  removeTechnology: PropTypes.func,
+  setTechnologiesError: PropTypes.func,
+  setAllErrors: PropTypes.func,
+  error: PropTypes.array,
+  technologiesError: PropTypes.object,
+  tech: PropTypes.object,
+  setTech: PropTypes.func,
+};
 
-  useEffect(() => {
-    axiosInstance
-      .get("/technologies")
-      .then((response) => {
-        const techs = response.data.technologies?.map((t) => t.name);
-        setDefaultTechs(techs);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }, []);
+export default function TechnologyForm({
+  defaultTechs,
+  technologies,
+  addTechnology,
+  removeTechnology,
+  setTechnologiesError,
+  technologiesError,
+  error,
+  setAllErrors,
+  tech,
+  setTech,
+}) {
+  const classes = useStyles();
 
   const handleChange = (e) => {
-    setErrors({});
-    setValue((prev) => ({ ...prev, [e.target.name]: e.target.value }));
-  };
-
-  const handleAdd = () => {
-    const errors = {};
-    if (!value.technology) {
-      errors.technology = "No puede ser vacío";
-    }
-    if (!value.seniority) {
-      errors.seniority = "No puede ser vacío";
-    }
-    if (Object.values(errors).length) {
-      setErrors(errors);
-      return;
-    }
-
-    const newDefaultTechs = defaultTechs.filter((t) => t !== value.technology);
-    setDefaultTechs(newDefaultTechs);
-    const { technology, seniority } = value;
-    selectedTechs.push({
-      type: [technology, seniority],
-      color: randomColor({ luminosity: "light" }),
-    });
-    setValue({ technology: "", seniority: "" });
-    setSelectedTechs(selectedTechs);
-  };
-
-  const handleDelete = (type) => {
-    console.log(type);
-    console.log(selectedTechs);
-    const newSelectedTechs = selectedTechs.filter((t) => t.type[0] !== type[0]);
-    setSelectedTechs(newSelectedTechs);
-    defaultTechs.push(type[0]);
-    setDefaultTechs(defaultTechs);
+    setAllErrors({});
+    setTechnologiesError({});
+    setTech((prev) => ({ ...prev, [e.target.name]: e.target.value }));
   };
 
   return (
     <Box id="technology-form" className={classes.container}>
-      <Grid container spacing={1} alignItems="center">
+      <Grid container spacing={1}>
         <Grid item xs={5}>
           <FormControl
             variant="outlined"
             fullWidth
-            error={!!errors?.technology}
+            error={!!technologiesError?.technology}
           >
             <InputLabel id="technology-select">Tecnologia</InputLabel>
-
             <Select
               label="Tecnologia"
               id="technology"
               labelId="technology-select"
               name="technology"
               onChange={handleChange}
-              value={value.technology}
+              value={tech.technology}
             >
               {defaultTechs?.map((option) => (
                 <MenuItem value={option}>{option}</MenuItem>
               ))}
             </Select>
-            <FormHelperText>{errors?.technology}</FormHelperText>
+            <FormHelperText>{technologiesError?.technology}</FormHelperText>
           </FormControl>
         </Grid>
         <Grid item xs={5}>
-          <FormControl variant="outlined" fullWidth error={!!errors?.seniority}>
+          <FormControl
+            variant="outlined"
+            fullWidth
+            error={!!technologiesError?.seniority}
+          >
             <InputLabel id="seniority-select">Seniority</InputLabel>
-
             <Select
               label="Seniority"
               id="seniority"
               labelId="seniority-select"
               name="seniority"
               onChange={handleChange}
-              value={value.seniority}
+              value={tech.seniority}
             >
               <MenuItem value="senior">Senior</MenuItem>
               <MenuItem value="semi-senior">Semi senior</MenuItem>
               <MenuItem value="junior">Junior</MenuItem>
             </Select>
-            <FormHelperText>{errors?.seniority}</FormHelperText>
+            <FormHelperText>{technologiesError?.seniority}</FormHelperText>
           </FormControl>
         </Grid>
-        <Grid item xs={2} alignItems="center" justifyContent="center">
-          <IconButton fullWidth color="primary" onClick={handleAdd}>
-            <AddIcon />
-          </IconButton>
+        <Grid item xs={2}>
+          <Box display="flex" justifyContent="center" mt={1}>
+            <IconButton fullWidth color="primary" onClick={addTechnology}>
+              <AddIcon />
+            </IconButton>
+          </Box>
         </Grid>
       </Grid>
+      <Box display="flex" justifyContent="center">
+        <Typography color="error">{error}</Typography>
+      </Box>
       <Box mt={2}>
         <Grid container spacing={1}>
-          {selectedTechs?.map((tech, index) => (
+          {technologies?.map((tech, index) => (
             <Grid key={`tech-${index}`} item>
-              <Chip tech={tech} onDelete={handleDelete} />
+              <Chip tech={tech} onDelete={removeTechnology} />
             </Grid>
           ))}
         </Grid>
@@ -136,26 +118,23 @@ export default function TechnologyForm({ }) {
   );
 }
 
-const Chip = memo(({ onDelete, tech }) => {
+const Chip = ({ onDelete, tech }) => {
+  const color = useMemo(() => randomColor({ luminosity: "light" }), [tech]);
   const capitalizeSeniority = {
     senior: "Senior",
     "semi-senior": "Semi senior",
     junior: "Junior",
   };
 
-  const { color, type } = tech;
-
   return (
     <MuiChip
-      style={{
-        backgroundColor: color,
-      }}
-      label={`${type[0]} - ${capitalizeSeniority[type[1]]}`}
+      style={{ backgroundColor: color }}
+      label={`${tech[0]} - ${capitalizeSeniority[tech[1]]}`}
       variant="outlined"
-      onDelete={() => onDelete(type)}
+      onDelete={() => onDelete(tech)}
     />
   );
-});
+};
 
 Chip.propTypes = {
   onDelete: PropTypes.func,
