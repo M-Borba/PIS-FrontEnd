e85da7b904aa0cd23c52044afa60539d83744d2c
Juancho Baz <45953832+juanbazpera@users.noreply.github.com>
2021-10-31 19:29:35 -0300

    Bug/person form fixes (#50)

--- src/containers/EditPerson/index.js
+++ src/containers/EditPerson/index.js
@@ -6,8 +6,6 @@ import React, { useState } from "react";
 import { axiosInstance } from "../../config/axios";
 import PersonForm from "../../components/PersonForm";
 import propTypes from "prop-types";
-import { rolesFormateados } from "../../config/globalVariables";
-import TechnologyHandler from "../../containers/PersonTechnologyHandler";
 
 EditPerson.propTypes = {
   personData: propTypes.shape({
@@ -30,12 +28,13 @@ export default function EditPerson({ personData, id, setNotify }) {
     working_hours: personData.working_hours,
     technologies: personData.technologies || [],
   });
+  const [errors, setErrors] = useState({});
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
     axiosInstance
-      .put("/people/" + id, {
+      .put(`/people/${id}`, {
         person: {
           first_name: person.first_name,
           last_name: person.last_name,
@@ -44,69 +43,27 @@ export default function EditPerson({ personData, id, setNotify }) {
           technologies: person.technologies,
         },
       })
-      .then((response) => {
-        if (response.status == 200)
-          setNotify({
-            isOpen: true,
-            message: `La persona ${personData.first_name} ${personData.last_name} se modifico con exito.`,
-            type: "success",
-            reload: true,
-          });
-        else
-          setNotify({
-            isOpen: true,
-            message: `Error inesperado.`,
-            type: "error",
-            reload: false,
-          });
+      .then(() => {
+        setNotify({
+          isOpen: true,
+          message: `La persona ${personData.first_name} ${personData.last_name} se modifico con exito.`,
+          type: "success",
+          reload: true,
+        });
       })
       .catch((error) => {
-        console.error(error.response);
-        if (error.response.status == 404) {
-          let message = error.response.data.error;
-          setNotify({
-            isOpen: true,
-            message: message,
-            type: "error",
-            reload: true,
-          });
-        } else {
-          let message = error.response.data.errors;
-          setNotify({
-            isOpen: true,
-            message: message[Object.keys(message)[0]],
-            type: "error",
-            reload: false,
-          });
-        }
+        setErrors(error.response.data?.errors);
       });
   };
 
-  const checkInput = (event) => {
-    if (event.target.id == "first_name")
-      setPerson({ ...person, first_name: event.target.value });
-    else if (event.target.id == "last_name")
-      setPerson({ ...person, last_name: event.target.value });
-    else if (event.target.id == "email")
-      setPerson({ ...person, email: event.target.value });
-    else if (event.target.id == "working_hours")
-      setPerson({ ...person, working_hours: parseInt(event.target.value) });
-  };
-
   return (
-    <div style={{ display: "flex", width: "100%" }}>
-      <PersonForm
-        onSubmit={(e) => handleSubmit(e)}
-        onInputChange={(e) => checkInput(e)}
-        person={person}
-        title={"Modificacion de Persona"}
-      />
-      <TechnologyHandler
-        techSelected={person.technologies}
-        setTechSelected={(techs) =>
-          setPerson({ ...person, technologies: techs })
-        }
-      />
-    </div>
+    <PersonForm
+      title={"Modificacion de Persona"}
+      onSubmit={handleSubmit}
+      person={person}
+      errors={errors}
+      setErrors={setErrors}
+      setPerson={setPerson}
+    />
   );
 }
