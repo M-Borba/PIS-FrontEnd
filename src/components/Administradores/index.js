142211e99ac0c0d2c0d568be4582fb9517f059d4
Stefano Graziani <stefa99graziani@gmail.com>
2021-11-04 18:28:56 -0300

    hotfix administradores

--- src/components/Administradores/index.js
+++ src/components/Administradores/index.js
@@ -149,10 +149,15 @@ export default function Administrador({ rows }) {
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
-        <Box m={1} mb={1} className={`${classes.rightBox} ${classes.box}`}>
-          <Button color="primary" variant="contained" onClick={handleAdminOpen}>
-            Agregar Administrador
-          </Button>
+        <Box className={classes.modal}>
+          <IconButton
+            aria-label="Close"
+            onClick={handleNewClose}
+            className={classes.closeButton}
+          >
+            <CloseIcon />
+          </IconButton>
+          <CreateAdministrator setNotify={setNotify} />
         </Box>
       </Modal>
       <DataGrid
