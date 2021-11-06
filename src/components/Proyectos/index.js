76bb38219b8f5b66e684f90cc44f1b81cd8ced4e
Stefano Graziani <stefa99graziani@gmail.com>
2021-11-06 15:16:49 -0300

    format

--- src/components/Proyectos/index.js
+++ src/components/Proyectos/index.js
@@ -108,17 +108,17 @@ export default function Proyecto({ rows, setRows }) {
       rows.map((row) =>
         row.id == projectData.id
           ? {
-            ...row,
-            name: projectData.name,
-            project_type: projectData.project_type,
-            project_state: projectData.project_state,
-            description: projectData.description,
-            budget: projectData.budget,
-            start_date: projectData.start_date,
-            end_date: projectData.end_date,
-            organization: projectData.organization,
-            technologies: projectData.technologies,
-          }
+              ...row,
+              name: projectData.name,
+              project_type: projectData.project_type,
+              project_state: projectData.project_state,
+              description: projectData.description,
+              budget: projectData.budget,
+              start_date: projectData.start_date,
+              end_date: projectData.end_date,
+              organization: projectData.organization,
+              technologies: projectData.technologies,
+            }
           : row
       )
     );
