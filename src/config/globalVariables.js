c9bd1ba8b6b0de79c3385444130f5cd4a37455ad
Camila Sanchez <csanchez@effectussoftware.com>
2021-12-13 16:25:52 -0300

    set backend host

--- src/config/globalVariables.js
+++ src/config/globalVariables.js
@@ -1,4 +1,5 @@
-export const BACKEND_HOST = "https://pis-es-backend-staging.herokuapp.com";
+export const BACKEND_HOST =
+  process.env.BACKEND_HOST || "https://operations-development.herokuapp.com/";
 export const NOT_LOGGED = "Aún no inició sesión";
 export const roles = [
   "Desarrollador",
