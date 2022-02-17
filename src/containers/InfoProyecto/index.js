f35abed7b1b15ab9b843ecdb0918dc144503fbf2
Gaston-Gonzalez <ggonzalez@effectussoftware.com>
2022-02-17 11:46:31 -0300

    InfoProyectoForm.js

--- src/containers/InfoProyecto/index.js
+++ src/containers/InfoProyecto/index.js
@@ -1,25 +1,12 @@
 import React, { useState } from "react";
 import propTypes from "prop-types";
-import { Box, Typography } from "@material-ui/core";
-import Grid from "@mui/material/Grid";
+import { Typography } from "@material-ui/core";
 import Divider from "@mui/material/Divider";
 import AssignmentIcon from "@mui/icons-material/Assignment";
 import { useSnackbar } from "notistack";
-import FormControl from "@mui/material/FormControl";
-import Select from "@mui/material/Select";
-import MenuItem from "@mui/material/MenuItem";
-import moment from "moment";
 
-import ListData from "./List";
-import AddButton from "../../components/AddButton";
-
-import { renderColor } from "../../utils/utils";
 import { axiosInstance } from "../../config/axios";
-import {
-  BUTTON_LABELS,
-  PROYECT_FORM_LABELS,
-} from "../../config/globalVariables";
-import TypographyStyled from "./TypographyStyled";
+import InfoProyectoForm from "./InfoProyectoForm";
 
 InfoProyecto.propTypes = {
   projectData: propTypes.object.isRequired,
@@ -86,162 +73,14 @@ export default function InfoProyecto({ projectData, type, onClose }) {
         </Typography>
       </div>
       <Divider style={{ marginBottom: 15, marginTop: 15 }} />
-      <form onSubmit={(event) => handleApplyChanges(event, projectData)}>
-        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-          <Grid item xs={6}>
-            <Box>
-              <TypographyStyled>
-                {PROYECT_FORM_LABELS.ORGANIZACION}:{" "}
-              </TypographyStyled>
-              <Typography display="inline" variant="body1">
-                {projectData.organization || "-"}
-              </Typography>
-            </Box>
-
-            <Box mt={2}>
-              <TypographyStyled>
-                {PROYECT_FORM_LABELS.TIPO_PROYECTO}:{" "}
-              </TypographyStyled>
-              <Typography display="inline" variant="body1">
-                {type}
-              </Typography>
-            </Box>
-
-            <Box mt={2} style={{ display: "flex", gap: "0.5rem" }}>
-              <TypographyStyled style={{ alignSelf: "center" }}>
-                {PROYECT_FORM_LABELS.ESTADO}:{" "}
-              </TypographyStyled>
-              <FormControl>
-                <Select
-                  labelId="state-input"
-                  label="Estado"
-                  id="project_state"
-                  name="project_state"
-                  style={{ width: "unset" }}
-                  value={project_state}
-                  onChange={(e) => {
-                    setProjectState(e.target.value);
-                  }}
-                >
-                  <MenuItem value="verde">{renderColor("verde")}</MenuItem>
-                  <MenuItem value="amarillo">
-                    {renderColor("amarillo")}
-                  </MenuItem>
-                  <MenuItem value="rojo">{renderColor("rojo")}</MenuItem>
-                  <MenuItem value="upcoming">
-                    {renderColor("upcoming")}
-                  </MenuItem>
-                </Select>
-              </FormControl>
-            </Box>
-
-            <Box mt={2}>
-              <TypographyStyled>
-                {PROYECT_FORM_LABELS.FECHA_INICIO}:{" "}
-              </TypographyStyled>
-              <Typography display="inline" variant="body1">
-                {moment(projectData.start_date).format("DD/MM/YYYY")}
-              </Typography>
-            </Box>
-            <Box mt={2}>
-              <TypographyStyled>
-                {PROYECT_FORM_LABELS.FECHA_FIN}:{" "}
-              </TypographyStyled>
-              <Typography display="inline" variant="body1">
-                {projectData.end_date
-                  ? moment(projectData.end_date).format("DD/MM/YYYY")
-                  : PROYECT_FORM_LABELS.INDEFINIDA}
-              </Typography>
-            </Box>
-
-            <Box mt={2}>
-              <TypographyStyled>Descripción: </TypographyStyled>
-              <Typography display="inline" variant="body1">
-                {projectData.description}
-              </Typography>
-            </Box>
-
-            {projectData.budget && (
-              <Box mt={2}>
-                <TypographyStyled>
-                  {PROYECT_FORM_LABELS.BUDGET}:{" "}
-                </TypographyStyled>
-                <Typography display="inline" variant="body1">
-                  {projectData.budget}
-                </Typography>
-              </Box>
-            )}
-          </Grid>
-
-          <Grid
-            item
-            style={{ display: "flex", padding: 0, flexDirection: "column" }}
-            xs={6}
-          >
-            <Box>
-              <ListData
-                title="Tecnologías"
-                type="tecnologías"
-                data={projectData.technologies}
-              />
-            </Box>
-            <Divider style={{ marginBottom: 15, marginTop: 15 }} />
-            <Box>
-              <ListData
-                title="Personas Asignadas"
-                type="personas"
-                data={projectData.people}
-              />
-            </Box>
-            <Grid
-              xs={12}
-              style={{
-                display: "flex",
-                flexDirection: "row",
-                width: "100%",
-              }}
-            >
-              <Grid
-                xs={12}
-                style={{
-                  display: "flex",
-                  flexDirection: "row",
-                  maxHeight: "45px",
-                  justifyContent: "flex-start",
-                  width: "100%",
-                  alignSelf: "flex-end",
-                }}
-              >
-                <AddButton type="submit" variant="contained">
-                  {BUTTON_LABELS.APPLY_CHANGES}
-                </AddButton>
-              </Grid>
-              <Grid
-                xs={6}
-                style={{
-                  display: "flex",
-                  flexDirection: "row",
-                  justifyContent: "flex-end",
-                  width: "100%",
-                  alignSelf: "flex-end",
-                }}
-              >
-                <AddButton
-                  styles={{
-                    backgroundColor: "#c21321",
-                    maxHeight: "45px",
-                    "&:hover": { backgroundColor: "#9d1e29" },
-                  }}
-                  variant="contained"
-                  onClick={onClose}
-                >
-                  {BUTTON_LABELS.CANCEL}
-                </AddButton>
-              </Grid>
-            </Grid>
-          </Grid>
-        </Grid>
-      </form>
+      <InfoProyectoForm
+        projectData={projectData}
+        handleApplyChanges={handleApplyChanges}
+        project_state={project_state}
+        setProjectState={setProjectState}
+        onClose={onClose}
+        type={type}
+      />
     </div>
   );
 }
